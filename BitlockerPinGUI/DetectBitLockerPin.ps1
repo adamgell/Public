0c381fb1585fb36a3596363f4b7202d5d3deeb46
Adam <27519+adamgell@users.noreply.github.com>
2025-09-16 13:18:23 -0400

    Add exit codes to DetectBitLockerPin script
    The script now exits with code 0 when both TPM PIN protector and log file are present, and code 1 otherwise. Removed unused code and comments related to BitLocker status detection for clarity.

--- BitlockerPinGUI/DetectBitLockerPin.ps1
+++ BitlockerPinGUI/DetectBitLockerPin.ps1
@@ -5,35 +5,17 @@ $hasTpmPin = $(Get-BitLockerVolume -MountPoint $env:SystemDrive).KeyProtector | 
 
 if ($hasTpmPin -and (Test-Path -Path $logFile)) {
     Write-Output "TPM PIN protector is present and log file exists at $logFile."
+    exit 0
 }
 elseif ($hasTpmPin) {
     Write-Output "TPM PIN protector is present, but log file does not exist."
+    exit 1
 }
 elseif (Test-Path -Path $logFile) {
     Write-Output "Log file exists at $logFile, but TPM PIN protector is not present."
+    exit 1
 }
 else {
     Write-Output "Neither TPM PIN protector is present nor log file exists."
+    exit 1
 }
-
-# PowerShell using Shell.Application COM object
-$shell = New-Object -ComObject Shell.Application
-$bitLockerStatus = $shell.NameSpace("C:").Self.ExtendedProperty("System.Volume.BitLockerProtection")
-if ($bitLockerStatus -eq 1) {
-    Write-Output "BitLocker is enabled on the C: drive."
-} else {
-    Write-Output "BitLocker is not enabled on the C: drive."
-}
-
-<#
-
-0 = Unencryptable
-1 = BitLocker enabled
-2 = BitLocker disabled
-3 = BitLocker encrypting
-4 = BitLocker decrypting
-5 = BitLocker suspended
-6 = BitLocker enabled and locked
-8 = BitLocker waiting for activation vbscript - .vbs script that echo's the Bitlocker status of the C Drive - Stack Overflow
-
-#>
